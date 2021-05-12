d31bef879cd6b2322273bc9786958d93b6b7d641
max <makschinasil@gmail.com>
2021-05-13 04:29:25 +0600

    Билет 4

---
+++ spring/src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    final
+    Repository repository;
+
+    public Controller(Repository repository) {
+        this.repository = repository;
+    }
+
+    //    CRUD read
+    @PatchMapping("/update/{id}")
+    public String update(@PathVariable Integer id, Bilet3 bilet3) {
+        if (repository.existsById(id)) {
+            repository.save(bilet3);
+            return "page";
+        }
+        else
+        {
+            return "page";
+        }
+    }
+}
---
+++ spring/src/main/java/com/example/demo/Repository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@org.springframework.stereotype.Repository
+public interface Repository extends JpaRepository<Bilet3, Integer> {
+}
