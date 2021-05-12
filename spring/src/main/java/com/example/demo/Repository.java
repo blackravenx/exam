dbae168e0c16617a6fe480c0dd8a4cca4efe0fbc
max <makschinasil@gmail.com>
2021-05-13 02:54:14 +0600

    Bilet 1

--- spring/src/main/java/com/example/demo/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@org.springframework.stereotype.Repository
-public interface Repository extends JpaRepository<Form, Integer> {
-}
