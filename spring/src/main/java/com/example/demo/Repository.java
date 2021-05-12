7225a50eafff922b89f15878fa91f1ba6e867b92
Max Shy <70589675+blackravenx@users.noreply.github.com>
2021-05-13 05:09:22 +0600

    Билет 10

--- spring/src/main/java/com/example/demo/Repository.java
+++ spring/src/main/java/com/example/demo/Repository.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+ 
 @org.springframework.stereotype.Repository
 public interface Repository extends JpaRepository<Bilet10, Integer> {
 
