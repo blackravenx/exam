7c63d70ca15d0ff181949473c91d677acc9d9665
Max Shy <70589675+blackravenx@users.noreply.github.com>
2021-05-13 22:19:41 +0600

    Билет 10

--- spring/src/main/java/com/example/demo/Controller.java
+++ spring/src/main/java/com/example/demo/Controller.java
@@ -12,7 +12,7 @@ public class Controller {
     public Controller(Repository repository) {
         this.repository = repository;
     }
-
+ 
     @GetMapping("/")
     public String lessThan5000(Model model) {
         model.addAttribute("pokupki", repository.findAllByCostLessThan(5000.0));
