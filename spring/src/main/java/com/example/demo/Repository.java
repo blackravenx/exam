a7631dbf1c4c7549de9be420b2b4b5b657f26a8b
Max Shy <70589675+blackravenx@users.noreply.github.com>
2021-05-13 22:18:51 +0600

    Билет 9

--- spring/src/main/java/com/example/demo/Repository.java
+++ spring/src/main/java/com/example/demo/Repository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
  
 @org.springframework.stereotype.Repository
-public interface Repository extends JpaRepository<Bilet10, Integer> {
+public interface Repository extends JpaRepository<Bilet9, Integer> {
 
     List<Bilet10> findAllByCostGreaterThan(Double cost);
 }
